64075131885c51d9c740e23218e23c765147b604
鄭羽芯 <beebite7@gmail.com>
2024-12-26 14:00:14 +0800

    init

---
+++ app.py
@@ -0,0 +1,145 @@
+from random import randint
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
+    Emoji,
+    ImageMessage,
+    VideoMessage,
+    AudioMessage,
+    LocationMessage,
+    TemplateMessage,
+    ButtonsTemplate,
+    PostbackAction,
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+import os
+
+app = Flask(__name__)
+
+configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN'))
+line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+@line_handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    text = event.message.text
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+
+        if text == '文字':
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text="這是文字訊息")]
+                )
+            )
+        
+        elif text == '表情符號':
+            emojis = [
+                Emoji(index=0, product_id="5ac1bfd5040ab15980c9b435", emoji_id="001"),
+                Emoji(index=12, product_id="5ac1bfd5040ab15980c9b435", emoji_id="002"),
+            ]
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text='$ LINE 表情符號 $', emojis=emojis)]
+                )
+            )
+        
+        elif text == '貼圖':
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(package_id="446", sticker_id="1988")]
+                )
+            )
+
+        elif text == '抽':
+            random = randint(1, 22)
+            print(random)
+            url = request.url_root + 'static/images/image' + str(random) + '.jpg'
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[
+                        ImageMessage(original_content_url=url, preview_image_url=url)
+                    ]
+                )
+            )
+
+        elif text == '影片':
+            random = randint(1, 22)
+            print(random) 
+            url = request.url_root + 'static/duck' + str(random) + '.mp4'
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[
+                        VideoMessage(original_content_url=url, preview_image_url=url)
+                    ]
+                )
+            )
+        
+        elif text == '唱歌':
+            url = request.url_root + 'static/duck.m4a'
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            duration = 6000
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[
+                        AudioMessage(original_content_url=url, duration=duration)
+                    ]
+                )
+            )
+
+        elif text == '想吃火鍋':
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[ # 22.617116, 120.289347 高雄愛河出海口，黃色小鴨位置
+                        LocationMessage(title='來吃築間!', address='築間幸福鍋物 中壢一號社宅店', latitude=24.9364459, longitude=121.2552889)
+                    ]
+                )
+            )
+
+if __name__ == "__main__":
+    app.run()
